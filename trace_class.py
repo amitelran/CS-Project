2551cc361b93739c5dbcfd5d9f285d1ef6034ddc
amitelran <amitelran2110@gmail.com>
2017-03-31 14:42:41 +0300

    Trace Class documented (Documenting in process)

--- trace_class.py
+++ trace_class.py
@@ -2,7 +2,6 @@
 #                               Trace Class
 # =============================================================================
 
-
 class Trace:
     def __init__(self, file_name, program_name, data):
         self.file_name = file_name
@@ -11,32 +10,41 @@ class Trace:
         # Variable to indicate if trace is malicious or benign
         self.is_malicious = True  # TODO get this value as parameter
 
+    # Display trace's file's name
     def display_file_name(self):
         print "Trace's file name: ", self.file_name
 
+    # Display trace's program name (1st line of trace's file)
     def display_program_name(self):
         print "Program Name: ", self.program_name
 
+    # Display trace's data
     def display_data(self):
         print "Trace Data: ", self.data
 
+    # Check whether trace is classified as benign or malicious
     def is_malicious_check(self):
         if self.is_malicious:
             print "Trace is malicious"
         else:
             print "Trace is not malicious"
 
+    # Change trace status from current status (as benign or malicious) to the opposite status when called
     def change_status(self):
         self.is_malicious = not self.is_malicious
 
+    # Trace's name getter (1st line of trace's file)
     def get_name(self):
         return self.program_name
 
+    # Trace's file's name getter
     def get_filename(self):
         return self.file_name
 
+    # Getting raw data
     def get_raw_data(self):
         return self.rawdata
 
+    # Parse raw data as string when called
     def get_data_as_string(self):
         return '*'.join([(str(call.get('API_Name')) + '@' + str(call.get('hKey'))) for call in self.raw_data[:]])
